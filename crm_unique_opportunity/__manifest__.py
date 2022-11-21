cf510705824a20cf33ff46a9a4aac2b8d5199897
ahmedalalaf <ahmedalalaf@gmail.com>
2022-11-21 14:09:26 +0300

    add unique_approtionty & sales order automation addons

---
+++ crm_unique_opportunity/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "crm_unique_opportunity",
+    'description': """
+        crm unique opportunity
+    """,
+    'author': "Ahmed Alalaf",
+    'category': 'crm',
+    'version': '15.0.1.0.0',
+    'depends': ['base', 'crm'],
+
+    # always loaded
+    'data': [
+    ],
+
+    'license': "AGPL-3",
+    'installable': True,
+
+}
